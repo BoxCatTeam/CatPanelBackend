e50e0c86f8f156b0e9600aebc96ce36dce78f8c9
soeur <juzi201314@gmail.com>
2022-12-31 17:20:17 +0800

    [feat][config]完成基础的读取配置文件,默认配置,合并/保存配置文件

---
+++ src/configure.rs
@@ -0,0 +1,93 @@
+use std::fs::File;
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use arc_swap::{ArcSwap, Guard};
+use figment::providers::{Env, Format, Json, Serialized, Toml};
+use figment::Figment;
+use once_cell::sync::OnceCell;
+use serde::{Deserialize, Serialize};
+
+static CONFIG: OnceCell<ArcSwap<Config>> = OnceCell::new();
+
+pub fn init_configure() -> anyhow::Result<()> {
+    if let Ok(path) = dotenv::dotenv() {
+        tracing::info!("load {}", path.display());
+    }
+    let config = reload()?;
+    CONFIG
+        .set(ArcSwap::from_pointee(config))
+        .expect("CONFIG意外的重复初始化");
+    Ok(())
+}
+
+#[inline]
+pub fn get_config() -> Guard<Arc<Config>> {
+    // SAFELY: 在程序一开始就应该已经调用`init_configure`
+    unsafe { CONFIG.get_unchecked() }.load()
+}
+
+fn reload() -> anyhow::Result<Config> {
+    Figment::new()
+        .merge(Toml::string(include_str!("default.toml")))
+        .merge(Json::file("_config_auto.json"))
+        .merge(Toml::file("config.toml"))
+        .merge(Env::prefixed("CP_"))
+        .extract::<Config>()
+        .map_err(Into::into)
+}
+
+fn merge<T>(target: T, persistence: bool) -> anyhow::Result<()>
+where
+    T: Serialize,
+{
+    let config = Figment::new()
+        .merge(Serialized::defaults(Config::clone(&get_config())))
+        .merge(Serialized::defaults(target))
+        .extract::<Config>()?;
+
+    if persistence {
+        serde_json::to_writer(File::create("_config_auto.json")?, &config)?;
+    }
+
+    // SAFELY: 在程序一开始就应该已经调用`init_configure`
+    unsafe { CONFIG.get_unchecked() }.store(Arc::new(config));
+
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Config {
+    http: HttpConfig,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct HttpConfig {
+    bind: SocketAddr,
+}
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Ok;
+    use serde_json::json;
+
+    use crate::configure::{get_config, init_configure, merge};
+
+    #[test]
+    fn test_merge() -> anyhow::Result<()> {
+        init_configure()?;
+        assert_eq!(get_config().http.bind.port(), 8686);
+
+        merge(
+            json!({
+                "http": {
+                    "bind": "127.0.0.0:65535",
+                },
+            }),
+            false,
+        )?;
+        assert_eq!(get_config().http.bind.port(), 65535);
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use crate::configure::init_configure;
 use tracing::info;
 
 use crate::log::init_tracing_subscriber;
 
+mod configure;
 mod log;
 
 #[global_allocator]
@@ -10,7 +12,9 @@ static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     init_tracing_subscriber();
+    init_configure()?;
 
     info!("Hello, world!");
+
     Ok(())
 }
